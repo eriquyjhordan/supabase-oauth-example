f504df6490ef0eb4719c448ba873223ced5916a5
Eriquy <eriquy@gmail.com>
2024-12-20 23:02:14 -0300

    chore: config eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,35 @@
-import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter, Poppins } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
+  variable: '--font-inter',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
 
 const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
-
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
 
 export const metadata: Metadata = {
-  title: "Supabase oauth example",
-  description: "Simple example of how to use Supabase OAuth with Google and Twitter",
-};
+  title: 'Supabase oauth example',
+  description:
+    'Simple example of how to use Supabase OAuth with Google and Twitter',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${inter.variable} ${poppins.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${poppins.variable} antialiased`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <div className="flex flex-1 justify-center align-center">
       <h1>Home</h1>
     </div>
-  );
+  )
 }
